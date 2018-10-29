9114d12b85572635892332555cfe2ee9ffc0319e
Abhihshek Pal <palabhishek322@gmail.com>
2018-10-30 00:46:12 +0530

    Add project files.

---
+++ SportsStoreFinal.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SportsStoreFinal.WebUI.MvcApplication" Language="C#" %>
---
+++ SportsStoreFinal.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,34 @@
+﻿using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStoreFinal.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
